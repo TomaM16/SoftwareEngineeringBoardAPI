b79dba9b77fce95dc268dd9068a2e0a89e83b336
TomaM16 <toma.milkov@gmail.com>
2021-10-25 23:47:58 +0300

    NorthStationBoardAPI; Add: project receiving information about trips and trains, when a request for API is made.

---
+++ Toma/homework2/homework2polls/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from homework2polls.models import Car, Board
+# Register your models here.
+admin.site.register(Car)
+admin.site.register(Board)
